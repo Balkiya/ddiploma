46f61c1297d6e50e06cb567336a5a408d796a7eb
Balkiya <balkiyakumisbaevaa.91@gmail.com>
2025-03-14 10:00:00 +0500

    b21

--- app/app.py
+++ app/app.py
@@ -5,3 +5,11 @@ from tensorflow.keras.models import load_model
 import os
 
 app = Flask(__name__)
+# === Загрузка моделей ===
+with open("models/gesture_type_model.pkl", "rb") as f:
+    type_model = pickle.load(f)
+
+with open("models/gesture_static_model.pkl", "rb") as f:
+    static_model = pickle.load(f)
+
+dynamic_model = load_model("models/lstm_dynamic_model.keras")
